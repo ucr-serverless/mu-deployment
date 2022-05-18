b981571da010b8cf048aee105b4aa3fec7e83373
ShixiongQi <qishixiong19931009@gmail.com>
2022-05-18 14:52:47 -0500

    FIXgit add 600-ko_install.sh  fix up ko installation script

--- 600-ko_install.sh
+++ 600-ko_install.sh
@@ -10,8 +10,14 @@ then
 	echo DOCKER_USER not defined
 	exit 1
 fi
+		
 
-#GO111MODULE=on go get github.com/google/ko/cmd/ko
-go get github.com/google/ko
+VERSION=0.7.1 # choose the latest version
+OS=Linux     # or Darwin
+ARCH=x86_64  # or arm64, i386, s390x
+curl -L https://github.com/google/ko/releases/download/v${VERSION}/ko_${VERSION}_${OS}_${ARCH}.tar.gz | tar xzf - ko
+sudo chmod +x ./ko
+sudo install ko /usr/bin
+
 echo "export KO_DOCKER_REPO='docker.io/$dockerhub_account'" >> ~/.bashrc
 echo "please source ~/.bashrc"
